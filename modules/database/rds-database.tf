c421936190edccbc81255a057bd06988137da2d2
josepamancio <jose@proinsight.tech>
2024-05-15 22:20:29 +0200

    database

--- modules/database/rds-database.tf
+++ modules/database/rds-database.tf
@@ -6,8 +6,8 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 5
   storage_type         = "gp2"
   engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
+  engine_version       = "5.7.30"
+  instance_class       = "db.t2.medium"
   username             = var.DB_USERNAME
   password             = var.DB_PASSWORD
   parameter_group_name = "default.mysql5.7"
